d0d6b76a8b156096e94401750ce49dd5a966a434
simonuks221 <simonasriauka@gmail.com>
2024-06-08 22:06:17 +0300

    feat: simple PC

--- VHDL-soft-core.ip_user_files/bd/main_design/sim/main_design.vhd
+++ VHDL-soft-core.ip_user_files/bd/main_design/sim/main_design.vhd
@@ -2,7 +2,7 @@
 --Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
---Date        : Sat Jun  8 19:15:16 2024
+--Date        : Sat Jun  8 22:04:38 2024
 --Host        : DESKTOP-5JRNHLO running 64-bit major release  (build 9200)
 --Command     : generate_target main_design.bd
 --Design      : main_design
@@ -24,7 +24,7 @@ entity main_design is
     RGB_R : out STD_LOGIC
   );
   attribute CORE_GENERATION_INFO : string;
-  attribute CORE_GENERATION_INFO of main_design : entity is "main_design,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=main_design,x_ipVersion=1.00.a,x_ipLanguage=VHDL,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=1,numPkgbdBlks=0,bdsource=USER,synth_mode=Hierarchical}";
+  attribute CORE_GENERATION_INFO of main_design : entity is "main_design,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=main_design,x_ipVersion=1.00.a,x_ipLanguage=VHDL,numBlks=3,numReposBlks=3,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=3,numPkgbdBlks=0,bdsource=USER,synth_mode=Hierarchical}";
   attribute HW_HANDOFF : string;
   attribute HW_HANDOFF of main_design : entity is "main_design.hwdef";
 end main_design;
@@ -42,6 +42,19 @@ architecture STRUCTURE of main_design is
     RGB_B : out STD_LOGIC
   );
   end component main_design_main_0_0;
+  component main_design_program_rom_impl_0_0 is
+  port (
+    CLK : in STD_LOGIC;
+    ADDR : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    DATA : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+  end component main_design_program_rom_impl_0_0;
+  component main_design_pc_0_0 is
+  port (
+    CLK : in STD_LOGIC;
+    P_COUNTER : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+  end component main_design_pc_0_0;
   signal BTN1_0_1 : STD_LOGIC;
   signal BTN2_0_1 : STD_LOGIC;
   signal CLK_IN_12MHz_0_1 : STD_LOGIC;
@@ -50,6 +63,8 @@ architecture STRUCTURE of main_design is
   signal main_0_RGB_B : STD_LOGIC;
   signal main_0_RGB_G : STD_LOGIC;
   signal main_0_RGB_R : STD_LOGIC;
+  signal pc_0_P_COUNTER : STD_LOGIC_VECTOR ( 7 downto 0 );
+  signal NLW_program_rom_impl_0_DATA_UNCONNECTED : STD_LOGIC_VECTOR ( 7 downto 0 );
 begin
   BTN1_0_1 <= BTN1;
   BTN2_0_1 <= BTN2;
@@ -69,5 +84,16 @@ main_0: component main_design_main_0_0
       RGB_B => main_0_RGB_B,
       RGB_G => main_0_RGB_G,
       RGB_R => main_0_RGB_R
+    );
+pc_0: component main_design_pc_0_0
+     port map (
+      CLK => CLK_IN_12MHz_0_1,
+      P_COUNTER(7 downto 0) => pc_0_P_COUNTER(7 downto 0)
+    );
+program_rom_impl_0: component main_design_program_rom_impl_0_0
+     port map (
+      ADDR(7 downto 0) => pc_0_P_COUNTER(7 downto 0),
+      CLK => CLK_IN_12MHz_0_1,
+      DATA(7 downto 0) => NLW_program_rom_impl_0_DATA_UNCONNECTED(7 downto 0)
     );
 end STRUCTURE;
---
+++ VHDL-soft-core.srcs/sources_1/new/pc.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity pc is
+  Port (
+    CLK: in STD_LOGIC;
+    P_COUNTER: out STD_LOGIC_VECTOR(7 downto 0)
+  );
+end pc;
+
+architecture Behavioral of pc is
+    signal internal_counter : integer range 0 to P_COUNTER'high := 0;
+begin
+    process(CLK)
+    begin
+      if rising_edge(CLK) then
+        internal_counter <= internal_counter + 1; --TODO: could be a problem that PC is delayed by 1
+        P_COUNTER <= std_logic_vector(to_unsigned(internal_counter, P_COUNTER'length));
+      end if;
+    end process;
+
+end Behavioral;
