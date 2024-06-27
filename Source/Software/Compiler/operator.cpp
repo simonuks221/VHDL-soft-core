7ea771348552ff22809b1e91e09a816a0c2c198c
simonuks221 <simonasriauka@gmail.com>
2024-06-27 18:03:34 +0300

    feat: working shunting yard with functions

--- Source/Software/Compiler/operator.cpp
+++ Source/Software/Compiler/operator.cpp
@@ -1,1 +1,13 @@
 #include "operator.hpp"
+
+std::string_view Operator::get_str() const {
+    return str;
+}
+
+unsigned int Operator::get_presedence() const {
+    return presedence;
+}
+
+bool Operator::get_left_associative() const {
+    return left_associative;
+}
