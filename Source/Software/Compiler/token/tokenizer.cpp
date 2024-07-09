44652d32f504cde23b57b655925df835b3cbed7f
simonuks221 <simonasriauka@gmail.com>
2024-07-09 19:12:45 +0300

    feat: tokenizer ignore comments

--- Source/Software/Compiler/token/tokenizer.cpp
+++ Source/Software/Compiler/token/tokenizer.cpp
@@ -16,21 +16,55 @@ static const std::unordered_map<std::string, IOperator*> operators = { //TODO: m
 
 bool Tokenizer::tokenize(std::ifstream &input_stream) {
     std::string temp_token = "";
-    char ch;
-    while(input_stream.get(ch)) {
-        if(isdigit(ch) || isalnum(ch)) {
-            temp_token += ch;
-            continue;
+    std::string line;
+    while (std::getline(input_stream, line)) {
+        temp_token.clear();
+        for (char ch : line) {
+            if(ch == '#') {
+                /* Found start of comment */
+                break;
+            }
+            if(isdigit(ch) || isalnum(ch)) {
+                temp_token += ch;
+                continue;
+            }
+            std::string ch_str(1, ch);
+            if(!temp_token.empty()) {
+                if(operators.find(temp_token) != operators.end()) {
+                    /* Operator in temp_token */
+                    IToken *new_token = operators.at(temp_token)->clone();
+                    token_list.push_back(new_token);
+                    temp_token.clear();
+                } else {
+                    /* No operator in temp_token */
+                    IToken *new_token = nullptr;
+                    bool is_all_num = std::all_of(temp_token.begin(), temp_token.end(), ::isdigit);
+                    if(is_all_num) {
+                        /* All numbers - constant */
+                        new_token = new Constant(temp_token);
+                    } else {
+                        /* Variable */
+                        new_token = new Variable(temp_token);
+                    }
+                    token_list.push_back(new_token);
+                    temp_token.clear();
+                }
+            }
+            if(operators.find(ch_str) != operators.end()) {
+                /* found operator */
+                IToken *new_token = operators.at(ch_str)->clone();
+                token_list.push_back(new_token);
+                temp_token.clear();
+            }
         }
-        std::string ch_str(1, ch);
         if(!temp_token.empty()) {
+            //TODO: duplicated code
             if(operators.find(temp_token) != operators.end()) {
                 /* Operator in temp_token */
                 IToken *new_token = operators.at(temp_token)->clone();
                 token_list.push_back(new_token);
-                temp_token.clear();
             } else {
-                /* No operator in temp_token */
+                /* No operator in temp_token*/
                 IToken *new_token = nullptr;
                 bool is_all_num = std::all_of(temp_token.begin(), temp_token.end(), ::isdigit);
                 if(is_all_num) {
@@ -41,34 +75,7 @@ bool Tokenizer::tokenize(std::ifstream &input_stream) {
                     new_token = new Variable(temp_token);
                 }
                 token_list.push_back(new_token);
-                temp_token.clear();
             }
-        }
-        if(operators.find(ch_str) != operators.end()) {
-            /* found operator */
-            IToken *new_token = operators.at(ch_str)->clone();
-            token_list.push_back(new_token);
-            temp_token.clear();
-        }
-    }
-    if(!temp_token.empty()) {
-        //TODO: duplicated code
-        if(operators.find(temp_token) != operators.end()) {
-            /* Operator in temp_token */
-            IToken *new_token = operators.at(temp_token)->clone();
-            token_list.push_back(new_token);
-        } else {
-            /* No operator in temp_token*/
-            IToken *new_token = nullptr;
-            bool is_all_num = std::all_of(temp_token.begin(), temp_token.end(), ::isdigit);
-            if(is_all_num) {
-                /* All numbers - constant */
-                new_token = new Constant(temp_token);
-            } else {
-                /* Variable */
-                new_token = new Variable(temp_token);
-            }
-            token_list.push_back(new_token);
         }
     }
     return true;
