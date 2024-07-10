042e6d77c221d9c618f233d43cc35b9a39024114
simonuks221 <simonasriauka@gmail.com>
2024-07-10 17:48:17 +0300

    feat: auto token assigning

--- Source/Software/Compiler/token/tokenizer.hpp
+++ Source/Software/Compiler/token/tokenizer.hpp
@@ -1,14 +1,18 @@
 #include "token.hpp"
+#include "operators.hpp"
 #include <vector>
 #include <fstream>
+#include <unordered_map>
 
 class Tokenizer {
     private:
         std::vector<IToken*> token_list;
+        std::unordered_map<std::string, IOperator*> operators;
     public:
         Tokenizer() {};
         ~Tokenizer() = default;
 
         std::vector<IToken*> & get_token_list(void);
         bool tokenize(std::ifstream &input_stream);
+        void add_operator(IToken* token);
 };
