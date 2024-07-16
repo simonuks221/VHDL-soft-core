f035878852a920722c8b9a60a4f7d73cba94d457
simonuks221 <simonasriauka@gmail.com>
2024-07-16 18:05:20 +0300

    feat: cfg node tree  traversal

--- Source/Software/Compiler/optimisation/data_flow_analysis.cpp
+++ Source/Software/Compiler/optimisation/data_flow_analysis.cpp
@@ -2,46 +2,66 @@
 #include <cassert>
 #include <algorithm>
 
-bool find_if_defined(IToken *token, ILine *token_line, std::vector<ILine *> &all_lines) {
-    for(ILine * line : all_lines) {
-        if(line == token_line) {
-            /* Reached token line, end */
+/* Traverses node map backwards to find if token was defined */
+IToken *find_if_defined(IToken *variable, CFGBlock *start_block) {
+    if((variable == nullptr) || (start_block == nullptr)) {
+        return nullptr;
+    }
+    /* Check if start block contains the variable in question */
+    auto it = std::find_if(start_block->line->get_variables_out().begin(), start_block->line->get_variables_out().end(), [variable](IToken* it_token) {
+        if(it_token->get_type() != variable->get_type()) {
             return false;
         }
-
-        auto it = std::find_if(line->get_variables_out().begin(), line->get_variables_out().end(), [token](IToken* it_token) {
-            if(it_token->get_type() != token->get_type()) {
-                return false;
-            }
-            return it_token->get_str() == token->get_str();
-        });
-
-        if(it != line->get_variables_out().end()) {
-            /* Found defined variable */
-            return true;
+        return it_token->get_str() == variable->get_str();
+    });
+    if(it != start_block->line->get_variables_out().end()) {
+        /* Found defined variable */
+        return static_cast<IToken *>(*it);
+    }
+    /* Variable not found in start block */
+    for(CFGBlock *previous : start_block->previous) {
+        IToken *ret = find_if_defined(variable, previous);
+        if(ret != nullptr) {
+            return ret;
         }
     }
-    return false;
+    return nullptr;
 }
 
 void DataFlowAnalysis::analyze(std::vector<ILine *> &all_lines) {
+    if(all_lines.empty()) {
+        return;
+    }
+    CFGBlock *last_block = nullptr;
     for(ILine * line : all_lines) {
+        /* Construct control flow graph blocks into linked node map */
+        CFGBlock *new_block = new CFGBlock();
+        new_block->line = line;
+        if(root == nullptr) {
+            /* First block */
+            root = new_block;
+            last_block = new_block;
+            continue;
+        }
+        last_block->next.push_back(new_block);
+        new_block->previous.push_back(last_block);
         /* Find if all in variables were defined previously */
-        for(IToken * token : line->get_variables_in()) {
-            if(!find_if_defined(token, line, all_lines)) {
+        for(IToken *token : line->get_variables_in()) {
+            IToken *definition = find_if_defined(token, last_block);
+            if(definition == nullptr) {
+                /* Not defined */
                 std::cerr << "Can't find definition for variable: " << token->get_str() << std::endl;
                 assert(false);
             }
         }
-        //CFGBlock new_block;
-        switch(line->get_type()) {
-            case eLine::Default: {
+        last_block = new_block;
+        // switch(line->get_type()) {
+        //     case eLine::Default: {
 
-            } break;
-            case eLine::Assignation: {
+        //     } break;
+        //     case eLine::Assignation: {
 
-            } break;
-        }
-        //blocks.push_back(new_block);
+        //     } break;
+        // }
     }
 }
