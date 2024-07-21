8969920b27613d8354c9eb29fbe201abd30a3d64
simonuks221 <simonasriauka@gmail.com>
2024-07-21 12:36:05 +0300

    feat: while and if conditions pushing

--- Source/Software/Compiler/token/keywords.hpp
+++ Source/Software/Compiler/token/keywords.hpp
@@ -8,6 +8,7 @@ class CurlyBracesKeyword : public Keyword, public ITokenAssemblable {
         mutable std::string instruction;
         bool open = false;
         unsigned int brace_idx = 0;
+        CurlyBracesKeyword *other = nullptr;
     public:
         CurlyBracesKeyword(std::string _str);
         virtual ~CurlyBracesKeyword() = default;
@@ -18,20 +19,37 @@ class CurlyBracesKeyword : public Keyword, public ITokenAssemblable {
         bool get_open(void) const;
         unsigned int get_brace_idx(void) const;
         void set_brace_idx(unsigned int new_value);
+        void set_other(CurlyBracesKeyword *_other);
+        CurlyBracesKeyword *get_other(void) const;
 };
 
-class WhileKeyword : public Keyword, public ITokenAssemblable {
+class ConditionalKeyword : public Keyword, public ITokenAssemblable, public ITokenScopeTransition {
     private:
         mutable std::string instruction;
-        unsigned int brace_idx = 0;
+        CurlyBracesKeyword *start_brace;
+        bool should_repeat;
     public:
-        WhileKeyword(std::string _str) : Keyword(_str) {};
-        virtual ~WhileKeyword() = default;
+        ConditionalKeyword(std::string _str, bool _should_repeat) : Keyword(_str, 2, 1, false), should_repeat(_should_repeat), start_brace(nullptr) {};
+        virtual ~ConditionalKeyword() = default;
 
         IToken *clone(void) const override;
         std::string_view assemble_instruction(void) const override;
+        void shunting_yard_action(std::stack<IToken*> &operator_stack, std::vector<IToken*> &output, IToken *current) const override;
 
-        unsigned int get_brace_idx(void) const;
-        void set_brace_idx(unsigned int new_value);
-        bool parse_for_brace(std::span<IToken *> tokens);
+        bool parse_for_brace(std::span<IToken *> tokens) override;
+        CurlyBracesKeyword *get_start_barce(void) const override;
+        bool get_should_repeat(void) const override;
+};
+
+class GotoKeyword : public Keyword, public ITokenAssemblable {
+    private:
+        mutable std::string instruction;
+        CurlyBracesKeyword *destination = nullptr;
+    public:
+        GotoKeyword(void) : Keyword("GOTO", 0, 1, false) {};
+        virtual ~GotoKeyword() = default;
+
+        IToken *clone(void) const override;
+        std::string_view assemble_instruction(void) const override;
+        void set_destination(CurlyBracesKeyword *_destination);
 };
--- Source/Software/Compiler/token/tokenizer.cpp
+++ Source/Software/Compiler/token/tokenizer.cpp
@@ -29,7 +29,9 @@ CurlyBracesKeyword closed_braces("}");
 // Keyword if_cond("if");
 // Keyword true_cond("true");
 // Keyword false_cond("false");
-WhileKeyword while_cond("while");
+ConditionalKeyword while_cond("while", true);
+ConditionalKeyword for_cond("for", true); //TODO: for examplory purpose only
+ConditionalKeyword if_cond("if", false);
 
 void Tokenizer::add_operator(IToken* token) {
     IOperator *op = static_cast<IOperator *>(token);
@@ -115,7 +117,7 @@ bool Tokenizer::tokenize(std::ifstream &input_stream, std::vector<ILine *> &line
 
 bool Tokenizer::process_tokens(std::vector<ILine *> &lines) {
     /* Find all braces and index them */
-    std::stack<unsigned int> brace_indexes;
+    std::stack<std::pair<unsigned int, CurlyBracesKeyword *>> brace_indexes;
     unsigned int max_braces_index = 1;
     for(ILine *line : lines) {
         for(IToken *token : line->get_tokens()) {
@@ -128,37 +130,66 @@ bool Tokenizer::process_tokens(std::vector<ILine *> &lines) {
             }
             if(keyword->get_open()) {
                 keyword->set_brace_idx(max_braces_index);
-                brace_indexes.push(max_braces_index);
+                brace_indexes.push(std::make_pair(max_braces_index, keyword));
                 max_braces_index++;
             } else {
                 if(brace_indexes.empty()) {
                     std::cerr << "Invalid braces" << std::endl;
                     assert(false);
                 }
-                keyword->set_brace_idx(brace_indexes.top());
+                keyword->set_brace_idx(brace_indexes.top().first);
+                keyword->set_other(brace_indexes.top().second);
+                brace_indexes.top().second->set_other(keyword);
                 brace_indexes.pop();
             }
         }
     }
     if(!brace_indexes.empty()) {
-        std::cerr << "Invalid braces" << std::endl;
+        std::cerr << "Invalid braces at the end" << std::endl;
         assert(false);
     }
-    /* Find all jump operations */
+    /* Find all block transition keywords */
     for(ILine *line : lines) {
         for(IToken *token : line->get_tokens()) {
             if(token->get_type() != eToken::Keyword) {
                 continue;
             }
-            WhileKeyword *keyword = dynamic_cast<WhileKeyword *>(token);
-            if(keyword == nullptr) {
+            ITokenScopeTransition *transition = dynamic_cast<ITokenScopeTransition *>(token);
+            if(transition == nullptr) {
                 continue;
             }
-            if(!keyword->parse_for_brace(line->get_tokens())) {
+            if(!transition->parse_for_brace(line->get_tokens())) {
                 std::cerr << "Failed to parse for while braces " << std::endl;
                 assert(false);
                 return false;
             }
+            /* Insert start brace at the start of the line - so the jump tp it would envelop argument loading */
+            CurlyBracesKeyword *start_brace = transition->get_start_barce();
+            auto it = std::find(line->get_tokens().begin(), line->get_tokens().end(), start_brace);
+            if(it == line->get_tokens().end()) {
+                std::cerr << "Failed to refind brace in line" << std::endl;
+                assert(false);
+            }
+            line->get_tokens().erase(it);
+            line->get_tokens().insert( line->get_tokens().begin(), start_brace);
+
+            if(transition->get_should_repeat()) {
+                /* Add return to while at block end - search for the lines of block end brace */
+                IToken *brace_to_find = transition->get_start_barce()->get_other();
+                for(ILine *find_line : lines) {
+                    auto it = std::find(find_line->get_tokens().begin(), find_line->get_tokens().end(), brace_to_find);
+                    if(it == find_line->get_tokens().end()) {
+                        continue;
+                    }
+                    /* Make goto at the end of block to point to start condition */
+                    GotoKeyword *goto_keyword = new GotoKeyword();
+                    goto_keyword->set_destination(transition->get_start_barce());
+                    find_line->get_tokens().insert(it, goto_keyword);
+                    break;
+                } //TODO: clean up into functions
+            }
+
+            break; //TODO: will not work with 2 whiles in one line
         }
     }
     return true;
